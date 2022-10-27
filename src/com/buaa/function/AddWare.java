2da32e86c5c4ec1bd929d8417e88ed70f0bebd34
JQKonatsu <zwj1813@163.com>
2022-10-27 11:15:58 +0800

    update addWare in scs3

--- src/com/buaa/function/AddWare.java
+++ src/com/buaa/function/AddWare.java
@@ -1,9 +1,10 @@
 package com.buaa.function;
 
-import com.buaa.data.*;
+import com.buaa.data.Course;
+import com.buaa.data.Ware;
 import com.buaa.main.UserOperation;
 
-import java.io.File;
+import java.io.*;
 import java.nio.file.Files;
 import java.util.ArrayList;
 
@@ -22,26 +23,38 @@ public class AddWare extends Function {
         if (parameterList.size() != 2) {
             System.out.println("arguments illegal");
         } else {
-            Course currentCourse = UserOperation.getCurrentCourse();
-            String id = parameterList.get(0);
-            String inLocation = parameterList.get(1);
-            File file = new File(inLocation);
-            if (UserOperation.isNoUser()) {
-                System.out.println("not logged in");
-            } else if (UserOperation.isManager()) {
-                System.out.println("permission denied");
-            } else if (UserOperation.isNoCourse()) {
-                System.out.println("no course selected");
-            } else if (!Ware.isIdLegal(id, currentCourse.getId())) {
-                System.out.println("ware id illegal");
-            } else if (!file.exists()) {
-                System.out.println("ware file does not exist");
-            } else {
-                try {
-                    //Files.copy(inLocation, inLocation);
-                } catch (Exception e) {
-
+            try {
+                Course currentCourse = UserOperation.getCurrentCourse();
+                String id = parameterList.get(0);
+                String inLocation = parameterList.get(1);
+                if (UserOperation.isNoUser()) {
+                    System.out.println("not logged in");
+                } else if (!UserOperation.isManager()) {
+                    System.out.println("permission denied");
+                } else if (UserOperation.isNoCourse()) {
+                    System.out.println("no course selected");
+                } else if (!Ware.isIdLegal(id, currentCourse.getId())) {
+                    System.out.println("ware id illegal");
+                } else {
+                    File inFile = new File(inLocation);
+                    File rootOutFile = new File("./data/" + id + "/wares");
+                    if (!inFile.exists()) {
+                        System.out.println("ware file does not exist");
+                    } else {
+                        if (!rootOutFile.exists()) {
+                            rootOutFile.mkdirs();
+                        }
+                        OutputStream out = new FileOutputStream(rootOutFile + "/" + inFile.getName());
+                        try {
+                            Files.copy(inFile.toPath(), out);
+                            System.out.println("add ware success");
+                        } catch (Exception e) {
+                            System.out.println("ware file operation failed");
+                        }
+                    }
                 }
+            } catch (Exception e) {
+                System.out.println("unexpected error");
             }
 //            } else if (Data.isWareIdExist(id)) {
 //                System.out.println("ware id duplication");
