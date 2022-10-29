8bd3263d2cbb1048c4cf75de0d9b26d4fc14bbf6
JQKonatsu <zwj1813@163.com>
2022-10-29 20:20:39 +0800

    reload .iml file

--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,4 @@
 import com.buaa.main.MyScs;
-import com.buaa.main.UserOperation;
-
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Test {
     public static void main(String[] args) {
