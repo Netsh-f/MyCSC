8268906ee2fce139d94e1e3f94019f0cdca2d380
JQKonatsu <zwj1813@163.com>
2022-10-29 17:03:12 +0800

    debugging scs3 2210291703

---
+++ BUAAOOP/myPatpat.bat
@@ -0,0 +1,5 @@
+xcopy D:\code\ideaProject\my-scs\src D:\code\ideaProject\my-scs\BUAAOOP\3-21371193-张文津\src /S /Y
+del D:\code\ideaProject\my-scs\BUAAOOP\data /S /Q
+del 9m.cc
+patpat ta -judge 3-21371193-张文津 -pwd 20030419
+pause .
